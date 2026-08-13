8ce512c7b59ea38e664b0cffaaadaf4c6f38b90e
Samuel Abera <sami21.good.bad@gmail.com>
2026-08-13 12:38:03 +0300

    feat: update src/modules/authentication/auth.ts

--- backend/src/modules/authentication/auth.ts
+++ backend/src/modules/authentication/auth.ts
@@ -3,6 +3,7 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "../../infrastructure/prisma/client.js";
 
 export const auth = betterAuth({
+    baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3001/api/auth",
     database: prismaAdapter(prisma, {
         provider: "postgresql",
     }),
@@ -15,5 +16,7 @@ export const auth = betterAuth({
         "http://localhost:5000",
         "http://localhost:3000",
         "http://localhost:5173",
+        "http://localhost:3001",
+        "http://localhost:5001"
     ],
 });
